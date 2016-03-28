c471ba208ef79dd6a587bf93a02743691040c2eb
Zane Hooper <zanehoop@gmail.com>
2016-03-27 20:02:38 -0400

    Rescaling Agents via harmonic motion

---
+++ Assets/PulseSize.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PulseSize : MonoBehaviour {
+
+	public float damping = 0.08f;
+	Vector3 originalSize;
+
+	// Use this for initialization
+	void Start () {
+		originalSize = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = Vector3.Slerp(
+			transform.localScale,
+			originalSize + damping * Vector3.one * Mathf.Cos(Time.time),
+			Time.deltaTime
+		);
+	}
+}
