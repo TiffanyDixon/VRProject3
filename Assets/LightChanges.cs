31463836925f031f1981d5c3120106f7f2020903
TiffanyDixon <TiffanyDixon94@gmail.com>
2016-03-29 10:29:37 -0400

    Light changing script

---
+++ Assets/LightChanges.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LightChanges : MonoBehaviour {
+	public float duration = 1.0F;
+	public Color firstColor = Color.red;
+	public Color secondColor = Color.blue;
+	public float intensity;
+	public Light lt;
+
+	// Use this for initialization
+	void Start () {
+		lt = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update (float intensity) {
+		float t = Mathf.PingPong (Time.time, duration) / duration;
+		lt.color = Color.Lerp (firstColor, secondColor, t);
+		lt.intensity = intensity;
+	
+	}
+}
