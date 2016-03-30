b23e0d54bf9a79bc48f09140ab334d9721d38768
Zane Hooper <zanehoop@gmail.com>
2016-03-30 18:49:01 -0400

    Make audience volume increase when player enters stage

---
+++ Assets/IncreaseVolumeOnEnter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncreaseVolumeOnEnter : MonoBehaviour {
+
+	public float finalVolume = 1f;
+	private bool entered = false;
+	private float startVolume;
+	private AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+		sound = GetComponent<AudioSource>();
+		startVolume = sound.volume;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float targetVolume = entered ? finalVolume : startVolume;
+		sound.volume = Mathf.Lerp(sound.volume, targetVolume, Time.deltaTime);
+	}
+
+	void OnTriggerEnter() {
+		entered = true;
+		Debug.Log("test");
+	}
+}
