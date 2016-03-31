393a68dca5299e4bce78836a2b1f885c4864efae
Zane Hooper <zanehoop@gmail.com>
2016-03-31 03:00:33 -0400

    Spotlights following player

---
+++ Assets/SpotlightFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpotlightFollow : MonoBehaviour {
+
+	private GameObject[] spotlights;
+	private bool hasEntered = false;
+
+	// Use this for initialization
+	void Start () {
+		spotlights = GameObject.FindGameObjectsWithTag("Stagelight");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasEntered) return;
+		foreach (GameObject spotlight in spotlights) {
+			spotlight.transform.LookAt(transform);
+		}
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		hasEntered = hasEntered || collider.tag == "Spotlight Collider";
+	}
+}
